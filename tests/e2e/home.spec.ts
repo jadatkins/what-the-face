fa83b044eb4bc62de4606c267a86019d574ce7b2
Alexander Atkins <jalexanderdatkins@gmail.com>
2026-06-27 02:32:14 +0100

    Revert "ci: what does it look like with failing tests?"
    This reverts commit 37f40c081ba4ced94b63761621353252da419131.

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from "@playwright/test";
 test("homepage smoke test", async ({ page }) => {
   await page.goto("/");
 
-  await expect(page.getByText("What's next?")).toBeHidden();
+  await expect(page.getByText("What's next?")).toBeVisible();
 });
